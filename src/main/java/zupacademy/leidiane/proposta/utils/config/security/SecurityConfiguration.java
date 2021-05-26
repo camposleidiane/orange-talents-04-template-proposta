a1b7fdd6e5480965a67ee6b9243c153f6fd09b45
camposleidiane <leidiane.campos@zup.com.br>
2021-05-26 17:08:01 -0300

    [100] Rodando Prometheus

--- src/main/java/zupacademy/leidiane/proposta/utils/config/security/SecurityConfiguration.java
+++ src/main/java/zupacademy/leidiane/proposta/utils/config/security/SecurityConfiguration.java
@@ -19,7 +19,7 @@ public class SecurityConfiguration  extends WebSecurityConfigurerAdapter {
                                 .antMatchers(HttpMethod.POST, "/api/propostas/**").hasAuthority("SCOPE_escopo-proposta")
                                 .antMatchers(HttpMethod.GET, "/api/cartoes/**").hasAuthority("SCOPE_escopo-proposta")
                                 .antMatchers(HttpMethod.POST, "/api/cartoes/**").hasAuthority("SCOPE_escopo-proposta")
-                                .antMatchers(HttpMethod.GET, "/actuator/**").hasAuthority("SCOPE_escopo-proposta")
+                                .antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
